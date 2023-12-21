acd9402bc17a16e653a5ce565c590d7bd854ec44
p.belezov <p.belezov@mfc38.ru>
2023-12-21 17:29:55 +0800

    Fixed background animation

--- resources/views/Caesar.vue
+++ resources/views/Caesar.vue
@@ -116,6 +116,19 @@ export default {
     height: 100vh;
 }
 
+@keyframes gradient {
+    0% {
+        background-position: 0 50%;
+    }
+    50% {
+        background-position: 100% 50%;
+    }
+    100% {
+        background-position: 0 50%;
+    }
+}
+
+
 .customBtn {
     background-color: #0d47a1;
     color: mintcream;
